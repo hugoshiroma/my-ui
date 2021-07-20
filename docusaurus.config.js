3136b1d69d035507ab76996ef74827e10860b21f
Hugo Gaio Shiroma <hugo.shiroma@acesso.com>
2021-07-20 01:46:13 -0300

    gh-pages configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hugoshiroma.github.io/my-ui/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
